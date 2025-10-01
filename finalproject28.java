90b27f9a1367c8636d67a3b782939273dcdf7468
rhenzarg <rhenzarg@gmail.com>
2025-10-01 14:12:29 +0700

    user 1

--- finalproject28.java
+++ finalproject28.java
@@ -2,5 +2,6 @@ public class finalproject28 {
 
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
+        System.out.println("User 1 menambahkan baris baru");
     }
 }

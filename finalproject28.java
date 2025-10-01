3d9138a0e9220d2967153f8f9c8ca96073377158
Adamavito12 <email.lgadam20@gmail.com>
2025-10-01 14:24:05 +0700

    user 2

--- finalproject28.java
+++ finalproject28.java
@@ -2,6 +2,8 @@ public class finalproject28 {
 
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
-        System.out.println("User 1 menambahkan baris baru");
+        System.out.println("User 2 menambahkan baris baru");
+        
+
     }
 }
